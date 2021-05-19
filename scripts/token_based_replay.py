7effc948016a35df8f0b9f34b6bdf1a7c98f6617
yuripaoloni <yuri.paoloni3@gmail.com>
2021-05-19 09:45:35 +0200

    updated token based replay

--- scripts/token_based_replay.py
+++ scripts/token_based_replay.py
@@ -1,21 +1,22 @@
 import sys
 from import_xes import import_xes
-from pm4py.algo.discovery.alpha import algorithm as alpha_miner
+from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
 from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
-from random import seed, randint
+from random import randint
 from pprint import pprint
 
 
 def run_token_replay(event_log):
-    net, initial_marking, final_marking = alpha_miner.apply(
+    net, initial_marking, final_marking = heuristics_miner.apply(
         event_log)
 
     traces = []
 
     # Randomly pick 5 traces in the events log
-    seed(1)
     for _ in range(5):
         value = randint(0, len(event_log))
+        print("\nTrace number: {}".format(value))
+        print(*event_log[value], end="\n", sep="\n")
         traces.append(event_log[value])
 
     replayed_traces = token_replay.apply(
